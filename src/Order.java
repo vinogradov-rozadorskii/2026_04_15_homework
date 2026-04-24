90083bf0695202b79a0315286381a17266fc6f6a
ivan <vinogradovrozadorsyi@gmail.com>
2026-04-24 22:17:24 +0300

    fix review comments

--- src/Order.java
+++ src/Order.java
@@ -6,6 +6,11 @@ public class Order {
     String customer;
     Product[] basket;
 
+    public Order(String customer, Product[] basket) {
+        this.customer = customer;
+        this.basket = basket;
+    }
+
     @Override
     public String toString() {
         return "Order{" +
@@ -14,16 +19,50 @@ public class Order {
                 '}';
     }
 
-    public Order(String customer, Product[] basket) {
-        this.customer = customer;
-        this.basket = basket;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
         Order order = (Order) o;
-        return Objects.equals(customer, order.customer) && Objects.deepEquals(basket, order.basket);
+
+        if (!Objects.equals(customer, order.customer)) {
+            return false;
+        }
+
+        if (basket == null && order.basket == null) {
+            return true;
+        }
+
+        if (basket == null || order.basket == null) {
+            return false;
+        }
+
+        if (basket.length != order.basket.length) {
+            return false;
+        }
+
+        for (int i = 0; i < basket.length; i++) {
+
+            if (basket[i] == null && order.basket[i] == null) {
+                continue;
+            }
+
+            if (basket[i] == null || order.basket[i] == null) {
+                return false;
+            }
+
+            if (!basket[i].equals(order.basket[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
